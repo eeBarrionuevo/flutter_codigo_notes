cdb31b4e932ee3e281c70512da020216c4a8bb5f
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-07 22:17:15 -0500

    List 2.2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/pages/detail_note_page.dart';
 import 'package:notes/ui/widgets/item_list_widget.dart';
@@ -10,12 +11,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final CollectionReference _notesReference = FirebaseFirestore.instance.collection('notes');
+
+
   @override
   Widget build(BuildContext context) {
+
     Orientation orientation = MediaQuery.of(context).orientation;
 
+    // _notesReference.get().then((QuerySnapshot value){
+    //   QuerySnapshot collection = value;
+    //   print(collection.size);
+    //   print(collection.docs);
+    // });
+    //
+    // _notesReference.snapshots().listen((event) {
+    //   print("wwwwwwwwwwww ${event.size}");
+    // });
+
     return Scaffold(
-      backgroundColor: Color(0xffF9FDFF),
+      backgroundColor: const Color(0xffF9FDFF),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> DetailNotePage()));
+        },
+        child: Icon(Icons.add),
+      ),
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
