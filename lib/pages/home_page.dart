34889cccc3ffdec1acb1f8b44abe2c9265385afd
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-07 21:20:08 -0500

    List 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -35,7 +35,53 @@ class _HomePageState extends State<HomePage> {
                 const SizedBox(
                   height: 14.0,
                 ),
-
+                Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.indigo,
+                            child: Icon(
+                              Icons.notifications_none,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12.0,
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                     fontWeight: FontWeight.bold,
+                                    color: Color(0xff323334).withOpacity(0.90),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 4.0,
+                                ),
+                                Text(
+                                  "25 de Sep, 2:42 PM",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Color(0xff323334).withOpacity(0.85),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
