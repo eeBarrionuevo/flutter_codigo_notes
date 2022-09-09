808cf9c56c1aba8178f2d78b8836659781531947
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-08 22:23:23 -0500

    List 4.6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/ui/general/colors.dart';
 import 'package:notes/ui/widgets/button_normal_widget.dart';
@@ -13,6 +14,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  _registerUser() async {
+    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: "mandarina@gmail.com",
+      password: "3volution",
+    );
+    print(userCredential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +58,19 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(
                   height: 12.0,
                 ),
-                ButtonNormalWidget(text: "Iniciar Sesión", onPressed: () {}),
+                ButtonNormalWidget(
+                  text: "Iniciar Sesión",
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                ButtonNormalWidget(
+                  text: "Registrar",
+                  onPressed: () {
+                    _registerUser();
+                  },
+                ),
               ],
             ),
           ),
