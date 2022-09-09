e6aa36993def77afda77915e643f292469c3af8d
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-09-08 19:25:20 -0500

    List 2.6

--- lib/pages/detail_note_page.dart
+++ lib/pages/detail_note_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:notes/ui/general/colors.dart';
 
 class DetailNotePage extends StatefulWidget {
   @override
@@ -31,8 +32,53 @@ class _DetailNotePageState extends State<DetailNotePage> {
   Widget build(BuildContext context) {
     print("BUILD");
     return Scaffold(
-      body: Center(
-        child: Text("Detail"),
+      backgroundColor: kBackgroundPrimaryColor,
+      appBar: AppBar(
+        backgroundColor: kFontPrimaryColor,
+        title: Text("Registrar nota"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text("Título",),
+              const SizedBox(
+                height: 6.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black87.withOpacity(0.05),
+                      blurRadius: 12,
+                      offset: const Offset(4, 4),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Título",
+                    hintStyle: TextStyle(
+                        fontSize: 14.0,
+                        color: kFontPrimaryColor.withOpacity(0.5)
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
